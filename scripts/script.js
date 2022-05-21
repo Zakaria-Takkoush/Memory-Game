1eeab41c2fb5476d53e529e2fd23af3ffbcbc82a
Zakaria <zaktakk96@gmail.com>
2022-05-21 17:52:54 +0300

    V1.3 - Declared main variables

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// declare box vairables
+var game = document.getElementById("game_box")
+var green = document.getElementById("green_box")
+var red = document.getElementById("red_box")
+var yellow = document.getElementById("yellow_box")
+var blue = document.getElementById("blue_box")
