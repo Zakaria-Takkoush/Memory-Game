df0681c83b0484e9896f4a820f4f61243d35d7ae
Zakaria <zaktakk96@gmail.com>
2022-05-21 18:15:08 +0300

    V1.4 - Declared audio variables

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,12 @@ var green = document.getElementById("green_box")
 var red = document.getElementById("red_box")
 var yellow = document.getElementById("yellow_box")
 var blue = document.getElementById("blue_box")
+
+// sound variables
+var green_sound = new Audio("assets\sounds\green.mp3")
+var red_sound = new Audio("assets\sounds\red.mp3")
+var yellow_sound = new Audio("assets\sounds\yellow.mp3")
+var blue_sound = new Audio("assets\sounds\blue.mp3")
+
+// sound event listerners
+green.addEventListener("click", green_sound.play)
